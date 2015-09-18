2527669f52ac9075beef224f9ee225f3ef857764
Scotty Jacobson <ScottyMJacobson@gmail.com>
2015-09-18 12:52:16 -0400

    Add setup.py for tox compatibility, add tox stuff to .gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-email-or-username',
+      version='0.1dev',
+      description='Example implementation of a django app with a custom auth backend that allows you to login with (case-insensitive!) email or username',
+      author='Scotty M Jacobson',
+      author_email='ScottyMJacobson@gmail.com',
+      url='https://github.com/ScottyMJacobson/django-email-or-username'
+      )
